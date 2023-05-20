9ad9631c196f980c4153011c461bf585c40e3426
Gaeun Jung <gaeunjung@Gaeuns-MBP.attlocal.net>
2023-05-20 03:48:17 -0700

    midterm

--- spring-cashier/README.md
+++
@@ -1,6 +0,0 @@
-# User Registration with Sprint Boot.
-
-## Based on the following tutorial:
-
-* https://studygyaan.com/spring-boot/login-register-example-using-spring-boot
-
--- spring-cashier/src/main/java/com/example/springlogin/login/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.springlogin.login;
-
-import java.util.List;
-
-public interface UserService {
-    public void saveUser(User user);
-    public List<Object> isUserPresent(User user);
-}
